cfc734a8fd4a96f9a1a3a33208df8cd126863d8a
NicolasSanna <nico13sanna@gmail.com>
2021-09-28 10:53:51 +0200

    Menu Burger en Objet JavaScript et CSS

--- REMANIEMENT/public/js/main.js
+++ REMANIEMENT/public/js/main.js
@@ -1,1 +1,47 @@
-console.log('Bienvenue sur mon site !')
+function burgerManager() 
+{ 
+
+    this.construct = function() 
+    {
+        let burger = document.getElementById('Burger');
+
+        burger.addEventListener('click', this.burgerClickEvent);
+
+        let burgerOff = document.getElementById('BurgerOff');
+
+        burgerOff.addEventListener('click', this.burgerOffClickEvent)
+    };
+
+    this.burgerClickEvent = function() 
+    {
+        let menu = document.getElementById('Menu');
+        let burger = document.getElementById('Burger')
+
+        if(!menu.style.display || menu.style.display == 'none') 
+        {
+            burger.style.display = 'none';
+            menu.style.display = 'block';
+            return;
+        }
+        menu.style.display = 'none';
+    };
+
+    this.burgerOffClickEvent = function()
+    {
+
+        let menu = document.getElementById('Menu');
+        let burger = document.getElementById('Burger');
+
+        if (menu.style.display || menu.style.display == 'block')
+        {
+            burger.style.display = 'block';
+            menu.style.display = 'none';
+            return;
+        }
+        menu.style.display = 'none';
+    }
+
+    this.construct();
+}
+
+let burger = new burgerManager();
