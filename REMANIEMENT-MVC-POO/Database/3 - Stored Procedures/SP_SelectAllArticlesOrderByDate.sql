0fac11037febda886192888aa038f60549369800
NicolasSanna <nico13sanna@gmail.com>
2021-10-03 13:59:00 +0200

    Création page Forum, page Article avec CSS

--- REMANIEMENT-MVC-POO/Database/3 - Stored Procedures/SP_SelectAllArticlesOrderByDate.sql
+++ REMANIEMENT-MVC-POO/Database/3 - Stored Procedures/SP_SelectAllArticlesOrderByDate.sql
@@ -3,8 +3,10 @@ DROP PROCEDURE IF EXISTS SP_SelectAllArticlesOrderByDate //
 CREATE PROCEDURE SP_SelectAllArticlesOrderByDate ()
 BEGIN
 
-    SELECT *
+    SELECT art.id, art.title, art.content, art.user_id, art.category_id, art.creation_date, u.pseudo, cat.category
     FROM articles art
-    ORDER BY art.creation_date DESC;
+    INNER JOIN users u ON art.user_id = u.id
+    INNER JOIN categories cat ON art.category_id = cat.id
+    ORDER BY art.creation_♫date DESC;
 
 END //
