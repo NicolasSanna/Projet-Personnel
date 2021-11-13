2eac185121bac276fee671218fbe08ea69e7fe1c
NicolasSanna <nico13sanna@gmail.com>
2021-11-13 19:30:51 +0100

    Truncate dans les datas

--- REMANIEMENT-MVC-POO/Database/2 - Datas/datas.sql
+++ REMANIEMENT-MVC-POO/Database/2 - Datas/datas.sql
@@ -1,15 +1,18 @@
 USE histoire_philosophie;
 
+TRUNCATE grants;
 INSERT INTO grants (id, privilege)
 VALUES
 (1, 'Administrateur'),
 (2, 'Auteur'),
 (3, 'Inscrit');
 
+TRUNCATE categories;
 INSERT INTO categories (id, category)
 VALUES
 (1, 'Non classé');
 
+TRUNCATE users;
 INSERT INTO users (id, firstname, lastname, pseudo, email, password, inscription_date, grant_id)
 VALUES 
 (1, 'Supprimé', 'Supprimé', 'Supprimé', 'Supprimé', 'Supprimé', NOW(), 3);
