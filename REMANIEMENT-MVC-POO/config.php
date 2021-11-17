3715660429de43938a8c75f801442d9ff04824dd
NicolasSanna <nico13sanna@gmail.com>
2021-11-17 09:08:11 +0100

    Réécriture du fichier .htaccess

--- REMANIEMENT-MVC-POO/config.php
+++ REMANIEMENT-MVC-POO/config.php
@@ -5,7 +5,7 @@ define('PROJECT_DIR', __DIR__);
 define('CONTROLLER_DIR', PROJECT_DIR . '/controller');
 define('TEMPLATE_DIR', PROJECT_DIR . '/templates');
 
-define('SITE_BASE_URL', 'http://localhost:8000');
+define('SITE_BASE_URL', 'http://localhost');
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'huma_scientio');
