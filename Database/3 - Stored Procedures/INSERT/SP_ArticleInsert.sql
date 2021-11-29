42e173239a334d0682fdcba60347dd5957ec945c
NicolasSanna <nico13sanna@gmail.com>
2021-11-29 16:03:11 +0100

    SP Update avec Case WHEN THEN ELSE

--- Database/3 - Stored Procedures/INSERT/SP_ArticleInsert.sql
+++ Database/3 - Stored Procedures/INSERT/SP_ArticleInsert.sql
@@ -16,3 +16,22 @@ BEGIN
     END IF;
 
 END //
+
+-- Avec CASE WHEN THEN
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS SP_ArticleInsert //
+CREATE PROCEDURE SP_ArticleInsert (v_title VARCHAR(255), v_content TEXT, v_category_id INT, v_user_id INT, v_image VARCHAR(255))
+BEGIN
+    
+        INSERT INTO articles (title, content, category_id, user_id, creation_date, image) 
+        VALUES (v_title, v_content, v_category_id, v_user_id, NOW(),
+            (
+                CASE v_image 
+                    WHEN NULL THEN NULL 
+                    ELSE v_image 
+                END
+            )
+        );
+
+END //
