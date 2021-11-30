71f24c24abe2be1ea5c8d512a3ba5753969671ec
NicolasSanna <nico13sanna@gmail.com>
2021-11-30 19:33:17 +0100

    Case

--- Database/3 - Stored Procedures/INSERT/SP_ArticleInsert.sql
+++ Database/3 - Stored Procedures/INSERT/SP_ArticleInsert.sql
@@ -1,23 +1,4 @@
-DELIMITER //
-DROP PROCEDURE IF EXISTS SP_ArticleInsert //
-CREATE PROCEDURE SP_ArticleInsert (v_title VARCHAR(255), v_content TEXT, v_category_id INT, v_user_id INT, v_image VARCHAR(255))
-BEGIN
-
-    IF (v_image IS NOT NULL) THEN
-    
-        INSERT INTO articles (title, content, category_id, user_id, creation_date, image) 
-        VALUES (v_title, v_content, v_category_id, v_user_id, NOW(), v_image);
-
-    ELSE
-
-        INSERT INTO articles (title, content, category_id, user_id, creation_date) 
-        VALUES (v_title, v_content, v_category_id, v_user_id, NOW());
-
-    END IF;
-
-END //
-
--- Avec CASE WHEN THEN
+-- Avec CASE WHEN THEN ELSE
 
 DELIMITER //
 DROP PROCEDURE IF EXISTS SP_ArticleInsert //
@@ -29,6 +10,25 @@ BEGIN
             (
                 CASE v_image 
                     WHEN NULL THEN NULL 
+                    ELSE v_image 
+                END
+            )
+        );
+
+END //
+
+-- Avec CASE WHEN THEN ELSE
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS SP_ArticleInsert //
+CREATE PROCEDURE SP_ArticleInsert (v_title VARCHAR(255), v_content TEXT, v_category_id INT, v_user_id INT, v_image VARCHAR(255))
+BEGIN
+    
+        INSERT INTO articles (title, content, category_id, user_id, creation_date, image) 
+        VALUES (v_title, v_content, v_category_id, v_user_id, NOW(),
+            (
+                CASE
+                    WHEN v_image IS NULL THEN NULL
                     ELSE v_image 
                 END
             )
--- Database/3 - Stored Procedures/UPDATE/SP_ArticleUpdate.sql
+++ Database/3 - Stored Procedures/UPDATE/SP_ArticleUpdate.sql
@@ -1,34 +1,6 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS SP_ArticleUpdate //
-CREATE PROCEDURE SP_ArticleUpdate(v_article_id INT(11), v_user_id INT(11), v_new_title VARCHAR(255), v_new_content TEXT, v_category_id INT(11), v_image VARCHAR(255))
-BEGIN
-
-
-    IF (v_image IS NULL) THEN
-
-        UPDATE articles
-        SET title = v_new_title,
-            content = v_new_content,
-            category_id = v_category_id,e
-        WHERE id = v_article_id
-        AND user_id = v_user_id;
-
-    ELSE
-
-        UPDATE articles
-        SET image = v_image
-        WHERE id = v_article_id
-        AND user_id = v_user_id;
-
-    END IF;
-
-END //
-
--- A essayer avec la syntaxe CASE-WHEN-THEN.
-
-DELIMITER //
-DROP PROCEDURE IF EXISTS SP_ArticleUpdate //
-CREATE PROCEDURE SP_ArticleUpdate(v_article_id INT(11), v_user_id INT(11), v_new_title VARCHAR(255), v_new_content TEXT, v_category_id INT(11), v_image VARCHAR(255))
+CREATE PROCEDURE SP_ArticleUpdate(v_article_id INT, v_user_id INT, v_new_title VARCHAR(255), v_new_content TEXT, v_category_id INT, v_image VARCHAR(255))
 BEGIN
 
         DECLARE existeImage VARCHAR(500);
@@ -45,6 +17,34 @@ BEGIN
             image = 
                     (CASE v_image 
                         WHEN NULL THEN existeImage
+                        ELSE v_image 
+                    END)        
+        WHERE id = v_article_id
+        AND user_id = v_user_id;
+
+END //
+
+-- AVEC CASE WHEN THEN ELSE
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS SP_ArticleUpdate //
+CREATE PROCEDURE SP_ArticleUpdate(v_article_id INT, v_user_id INT, v_new_title VARCHAR(255), v_new_content TEXT, v_category_id INT, v_image VARCHAR(255))
+BEGIN
+
+        DECLARE existeImage VARCHAR(500);
+
+        SELECT image
+        INTO existeImage
+        FROM articles
+        WHERE id = v_article_id;
+
+        UPDATE articles
+        SET title = v_new_title,
+            content = v_new_content,
+            category_id = v_category_id,
+            image = 
+                    (CASE  
+                        WHEN v_image IS NULL THEN existeImage
                         ELSE v_image 
                     END)        
         WHERE id = v_article_id
