77f757a58f46b61aac1177cb990aff12f7c57334
NicolasSanna <nico13sanna@gmail.com>
2021-12-01 14:40:19 +0100

    indentation

--- library/functions.php
+++ library/functions.php
@@ -1,14 +1,12 @@
 <?php
 
 // La fonction asset permet de faire appel aux différents fichiers venant des dossiers du dossier public : css, js, img,... Elle prend en paramètre le chemin à partir de l'index.php.
-
 function asset(string $path): string 
 {
     return SITE_BASE_URL . '/' . $path;
 }
 
 // La fonction buildUrl prend en paramètre le nom de la route venant du tableau de routes de routes.php, en second paramètre, elle peut recevoir des éléments GET.
-
 function buildUrl(string $routeName, array $params = []): string
 {
     // Si la clé route venant de la constante ROUTES n'existe pas on retourne faux.
@@ -18,7 +16,6 @@ function buildUrl(string $routeName, array $params = []): string
     }
 
     // L'URL définie vient du path du nom de la route du tableau associatif de la constante ROUTES.
-
     $url = ROUTES[$routeName]['path'];
 
     // S'il y a des paramètres, (des $_GET dans l'URL notamment)
