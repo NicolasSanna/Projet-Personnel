8ed44cc2d88520c88490028cae2a930d9cad4636
NicolasSanna <nico13sanna@gmail.com>
2021-12-01 12:50:47 +0100

    ajout de commentaires sur les Model

--- src/Model/CategoryModel.php
+++ src/Model/CategoryModel.php
@@ -1,88 +1,128 @@
 <?php
 
+// Espace de nom App\Model (composer.json : src/Model)
 namespace App\Model;
 
+// On va chercher App\Framework\AbstractModel (composer.json : src/Framework/AbstractModel).
 use App\Framework\AbstractModel;
 
+// On créé la classe Category Model qui hérite grâce à extends des propriétés et des méthodes de l'Abstract Model. (Celle-ci créé l'objet Database auquel nous avons accès ici).
 class CategoryModel extends AbstractModel
 {
+    // Création de la méthode getOneCategory qui prend en paramètre l'identifiant nunmérique de la catégorie recherchée.
     public function getOneCategory(int $idOfCategory)
     {
+        // On créé une variable $sql qui appelle la procédure stockée en lui donnant le paramètre anonyme pour la préparation de la requête.
         $sql = 'CALL SP_CategorySelect(?)';
 
+        // On récupère dans la varaible $category l'opération à partir de la méthode getOneCategory de l'objet database.
         $category = $this->database->getOneResult($sql, [$idOfCategory]);
 
+        /// On retourne $category.
         return $category;
     }
 
+    // On créé une méthode getAllCategories(). Sauf la catégorie Non classée afin de ne pas la supprimer.
     public function getAllCategories()
     {
+        // On stocke dans la variable $sql l'appel à la procédure stockée correspondante.
         $sql = 'CALL SP_AllCategoriesSelect()';
 
+        // On récupère dans $categories l'exécution de l'opération depuis l'objet database et sa méthode getAllResults. On lui donne ne paramètre la requête SQL.
         $categories = $this->database->getAllResults($sql);
 
+        // On retourne le résultat dans $categories.
         return $categories;
     }
 
+    // On créé une méthode createCategory qui prend en paramètre une chaîne de caractères. C'est cette méthode que l'on appelle pour créer une nouvelle catégorie.
     public function createCategory(string $newCategory)
     {
+        // On stocke dans $sql l'appel de notre procédure stockée.
         $sql = 'CALL SP_CategoryInsert(?)';
 
+        // ON stocke dans insertNewcCategory le résultat de l'opération depuis la base de données.
         $insertNewCategory = $this->database->getOneResult($sql, [$newCategory]);
 
+        // On retourne le résultat de $insertNewCategory. (le résultat du message de la procédure stockée notamment).
         return $insertNewCategory;
     }
 
+    // On créé une fonction modifyCategory qui prend en paramètre le numéro d'identifiant de la table categories et la chaîne de caractère qui va remplacer l'ancien nom de la catégorie concernée.
     public function modifyCategory(int $idOfCategory, string $newCategoryName)
     {
+        // On créé une variable $sql qui va être la requête SQL. Elle appelle la procédure stockée.
         $sql =  'CALL SP_CategoryUpdate(?, ?)';
 
+        // On stocke dans $modifyCategory le résultat de l'opération dans l'objet database et sa méthode getOneResult. on lui donne les paramètres à la suite de la requête SQL à réaliser. 
         $modifyCategory = $this->database->getOneResult($sql, [$idOfCategory, $newCategoryName]);
 
+        // On retourne la variable $modifyCategory.
         return $modifyCategory;
     }
-
+    
+    // On créé une méthode getAllCategoriesForArticle(). Elle contient toutes les catégories. 
     public function getAllCategoriesForArticle()
     {
+        // On stocke dans $sql la requête SQL. La procédure stockée ici.
         $sql ='CALL SP_AllCategoriesForArticleSelect()';
 
+        // On récupère dans $allCategoriesForArticle le résultat de la méthode getAllResults et de la requête SQL dans l'objet database.
         $allCategoriesForArticle = $this->database->getAllResults($sql);
 
+        // On renvoie le résultat final.
         return $allCategoriesForArticle;
     }
 
+    // On créé une méthode deleteCategory qui prend en paramètre le numéro d'identifiant de la table categories.
     public function deleteCategory(int $idOfCategory)
     {
+        // On stocke dans $sql l'appel de la procédure stockée.
         $sql = 'CALL SP_CategoryDelete(?)';
 
+        // On stocke dans $deleteCategory le résultat de la méthode getOneResult de l'objet database.
         $deleteCategory = $this->database->getOneResult($sql, [$idOfCategory]);
 
+        // On renvoie le résultat.
         return $deleteCategory;
     }
 
+    // On créé une fonctionn deleteCategoryWithoutArticles. Elle prend en paramètre le numéro de la catégorie.
     public function deleteCategoryWithoutArticles(int $idOfCategory)
     {
+        // On stocke dans $sql l'appel de la procédure stockée en lui donnant le paramètre anonyme.
         $sql = 'CALL SP_CategoryWithoutArticlesDelete(?)';
 
+        // On range dans $deleteCategoryWithoutArticles le résultat de la méthode getOneResult avec $sql et l'identifiant en paramètre à supprimer.
         $deleteCategoryWithoutArticles = $this->database->getOneResult($sql, [$idOfCategory]);
 
+        // On retourne le résultat.
         return $deleteCategoryWithoutArticles;
     }
 
+    // On créé une méthode getAllCategoriesForForum().
     public function getAllCategoriesForForum()
     {
+        // On stocke dans $sql l'appel de la procédure stockée.
         $sql = 'CALL SP_AllCategoriesForArticleSelect()';
 
+        // On récupère dans la variable le résultat de la requête sql depuis la méthode getAllResult de l'objet database.
         $getAllCategoriesForForum = $this->database->getAllResults($sql);
+
+        // On retourne le résultat.
         return $getAllCategoriesForForum;
     }
 
+    // On créé une méthode getArticlesByCategory en prenant en paramètre le numéro d'identifiant de la catégorie concernée. 
     public function getArticlesByCategory(int $idOfCategory)
     {
+        // On stocke dans $sql la requête SQL à effectuer. Ici, l'appel d'une procédure stockée avec un paramètre anonyme.
         $sql = 'CALL SP_ArticlesByCategorySelect(?)';
 
+        // On stocke le résultat de la méthode getAllResults et de ses paramètre venant de l'objet database dans une variable.
         $getArticlesByCategory = $this->database->getAllResults($sql, [$idOfCategory]);
 
+        // On retourne le résultat.
         return $getArticlesByCategory;
     }
 }
--- src/Model/CommentModel.php
+++ src/Model/CommentModel.php
@@ -1,41 +1,63 @@
 <?php
 
+// Espace de nom App\Model (composer.json : src/App/Model).
 namespace App\Model;
 
+// On va utiliser App\Framework\AbstractModel (composer.json : src/Framework/AbstractModel).
 use App\Framework\AbstractModel;
 
+// On créé une classe CommentModel qui va hériter des propriétés de l'AbstractModel.
 class CommentModel extends AbstractModel
 {
+    // On créé une méthode addComment qui prend en paramètre la chaîne de caractères du commentaire, le numéro d'identifiant de l'utilisateur et le numéro d'identifiant de l'article.
     public function addComment(string $comment, int $userId, int $articleId)
     {
+        // On stocke la requête SQL. L'appel de la procédure stockée elle prend les trois paramètres anonymes : le commentaire, l'identifiant utilisateur, et le numéro d'article.
         $sql = 'CALL SP_CommentInsert(?, ?, ?)';
 
+        // On stocke dans insertComment le résultat de l'exécution de la procédure stockée depuis l'objet database et sa méthode executeQuery.
         $insertComment = $this->database->executeQuery($sql, [$comment, $userId, $articleId]);
 
+        // On retourne le résultat.
         return $insertComment;
     }
 
+    // On créé une fonction getAllComments qui prend en paramètre le numéro d'identifiant de l'article (tous les commentaires d'un seul article).
     public function getAllComments(int $articleId)
     {
+        // On stocke dans $sql la requête à effectuer, l'appel de la procédure stockée qui prend le numéro d'identifiant de l'article comme paramètre anonyme.
         $sql = 'CALL SP_AllCommentsArticleSelect(?)';
+
+        // On stocke le résultat dans $getAllComment à partir de la méthode getAllResults et des paramètres données pour son exécution dans l'objet database.
         $getAllcomment = $this->database->getAllResults($sql, [$articleId]);
+
+        // On récupère le résultat.
         return $getAllcomment;
     }
 
+    // On créé une fonction getAllCommentsNotApprouved().
     public function getAllCommentsNotApprouved()
     {
+        // On stocke dans $sql la requête à effectuer. Ici l'appel d'une procédure stockée.
         $sql = 'CALL SP_AllCommentsNotApprouvedSelect ()';
 
+        // On stocke dans getAllCommentsNotApprouved le résultat de la requête issue de la méthode getAllResults de l'objet database.
         $getAllCommentsNotApprouved = $this->database->getAllResults($sql);
 
+        // On retourne le résultat.
         return $getAllCommentsNotApprouved;
     }
 
+    // On créé une méthode getOneComment qui prend en paramètre le numéro d'identifiant du commentaire.
     public function getOneComment(int $commentId)
     {
+        // On stocke dans $sql l'appel de la procédure stockée qui prend le numéro d'identifiant comme paramètre anonyme. 
         $sql = 'CALL SP_OneCommentSelect(?)';
+
+        // On stocke le résultat de l'exécution de la requête dans l'objet database et sa méthode getOneResult.
         $getOneComment = $this->database->getOneResult($sql, [$commentId]);
 
+        // On retourne le résultat.
         return $getOneComment;
     }
 
