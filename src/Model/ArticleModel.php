7984bbe5a88229deced5bd80c29f1d0e6f458c5e
NicolasSanna <nico13sanna@gmail.com>
2021-12-27 18:46:18 +0100

    public function

--- src/Model/ArticleModel.php
+++ src/Model/ArticleModel.php
@@ -14,7 +14,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé une méthode getAllArticles().
      */
-    function getAllArticles()
+    public function getAllArticles()
     {
         // On range dans $sql la requête SQL à effectuer. Ici, l'appel d'une procédure stockée MySQL.
         $sql = 'CALL SP_AllArticlesOrderByDateSelect ()';
@@ -26,7 +26,7 @@ class ArticleModel extends AbstractModel
         return $articles;
     }
 
-    function getLastFiveArticles()
+    public function getLastFiveArticles()
     {
         $sql = 'CALL SP_FiveArticlesOrderByDateSelect()';
 
@@ -38,7 +38,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé une méthode getOneArticle qui prend en paramètre un nombre, l'identifiant de l'article.
      */
-    function getOneArticle(int $idOfArticle)
+    public function getOneArticle(int $idOfArticle)
     {
         // On range dans $sql la requête SQL à effectuer, ici, l'appel d'une procédure stockée avec un paramètre anonyme, l'identifiant de l'article.
         $sql = 'CALL SP_ArticleSelect(?)';
@@ -53,7 +53,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé une méthode insertArticle qui reçoit en paramètre le title, le content, l'identifiant de catégorie, l'identifiant utilisateur, et le nom de l'image qui par défaut est null car il est possible d'ajouter ou non une image. 
      */
-    function insertArticle(string $title, string $content, int $category_id, int $user_id, string $image = null)
+    public function insertArticle(string $title, string $content, int $category_id, int $user_id, string $image = null)
     {
 
         // On range dans $sql la chaîne représentant la requête SQL. Ici, l'appel d'une procédure stockée prenant en paramètre ce que la méthode a reçu.
@@ -69,7 +69,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé une méthode getMyArticles() elle prend en paramètre le numéro d'identifiant de l'utilisateur en session (userId).
      */
-    function getMyArticles(int $userId)
+    public function getMyArticles(int $userId)
     {
         // On range dans $sql la requête SQL qui permet de récupérer les articles de l'utilisateur grâce au paramètre donné à la procédure stockée MySQL.
         $sql = 'CALL SP_MyArticlesSelect(?)';
@@ -84,7 +84,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé une méthode deleteArticle qui prend en paramètre l'identifiant de l'article à supprimer et l'auteur de celui-ci.
      */
-    function deleteArticle(int $articleId, int $userId)
+    public function deleteArticle(int $articleId, int $userId)
     {
         // On range dans $sql la requête SQL qui prend en paramètre l'identifiant de l'article et son auteur qui veut le supprimer. Ici en procédure stockée.
         $sql = 'CALL SP_ArticleDelete(?, ?)';
@@ -99,7 +99,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé la méthode modifyArticle qui prend en paramètre le numéro d'identifiant de l'article, l'utilisateur, le nouveau titre, le nouveau contenu, la nouvelle catégorie et l'image. Par défaut, l'image est à null car on peut ne pas vouloir mettre d'image.
      */
-    function modifyarticle(int $articleId, int $userId, string $newtitle, string $newcontent, int $newcategory, string $image = null)
+    public function modifyarticle(int $articleId, int $userId, string $newtitle, string $newcontent, int $newcategory, string $image = null)
     {
         // On range dans $sql la requête SQL qui va réaliser l'opération, grâce aux paramètres qu'on lui donne.
         $sql = 'CALL SP_ArticleUpdate(?, ?, ?, ?, ?, ?)';
@@ -114,7 +114,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé une méthode searchArticle qui prend en paramètre une chaîne de caractères.
      */
-    function searchArticle(string $searchArticle)
+    public function searchArticle(string $searchArticle)
     {
         // On range dans $sql la requête SQL à effectuer dans la base de données et on lui donne en paramètre la chaine de caractèreq contenant ce que l'on désire chercher.
         $sql = 'CALL SP_SearchSelect(?)';
@@ -129,7 +129,7 @@ class ArticleModel extends AbstractModel
     /**
      * On créé une méthode deleteImageArticle qui prend en paramètre l'identifiant de l'article.
      */
-    function deleteImageArticle(int $idOfArticle)
+    public function deleteImageArticle(int $idOfArticle)
     {
         // On range dans $sql la requête SQL que l'on veut effectuer. Elle prend en paramètre l'identifiant de l'article dont on va retirer l'image.
         $sql = 'CALL SP_ArticleImageUpdate(?)';
