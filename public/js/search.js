8da4fb7705bfb553d7330b7f94a83ec4a9d59554
NicolasSanna <79462492+NicolasSanna@users.noreply.github.com>
2022-03-30 10:09:49 +0200

    secure

--- public/js/search.js
+++ public/js/search.js
@@ -1,24 +1,45 @@
 import {Tools} from './modules/tools.js'
 
+/**
+ * Récupération de l'élément HTML du formulaire par son id.
+ */
 const form = document.getElementById('search-form');
 
+/**
+ * Création de la classe SearchManager
+ */
 class SearchManager
 {
     constructor()
     {
 
+        /**
+         * On pose un écouteur d'événement sur le submit du formulaire, et on lance la méthode asynchrone
+         */
         form.addEventListener('submit', this.onSubmitFormSearch);
     }
 
     async onSubmitFormSearch(event)
     {
 
+        /**
+         * Arrêt du comportement par défaut du navigateur.
+         */
         event.preventDefault();
 
+        /**
+         * Création d'un objet de la classe FormData avec en paramètre les données du formulaire.
+         */
         const formData = new FormData(form);
 
+        /**
+         * Récupération de la valeur de l'input avec la méthode get suivie du nom de la clé.
+         */
         let search = formData.get('search');
         
+        /**
+         * Vérification des chaînes de caractères vides.
+         */
         if (search.trim() == '')
         {
             alert("Le champ de recherche est vide");
@@ -26,8 +47,14 @@ class SearchManager
         else
         {
 
+            /**
+             * Récupération de l'URL par l'attribut action du formulaire.
+             */
             const url = form.action; 
 
+            /**
+             * Tableau d'options : méthode, objet formData du formulaire, en-têtes.
+             */
             const options = 
             {
                 method: 'post',
@@ -39,12 +66,26 @@ class SearchManager
 
             }
 
+            /**
+             * Lancement de la requête AJAX vers le serveur (requête HTTP)
+             * Réception de la réponse du serveur et des données.
+             */
             const response = await fetch(url, options);
             const results = await response.json();
 
+            /**
+             * Récupération de la div qui va contenir les résultats de la recherche.
+             */
             const container = document.querySelector('.containerSearch');
+
+            /**
+             * On vide la div en cas de doublons (relance de la recherche).
+             */
             container.innerHTML = '';
 
+            /**
+             * Boucle de récupération des articles (génération des balises HTML, ajout des classes associées et appropriées)
+             */
             for (const result of results)
             {
 
